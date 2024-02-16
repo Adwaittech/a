bd7195d69cc6308a85d2697b5a3f384e9d6444ee
Adwaittech <154214209+Adwaittech@users.noreply.github.com>
2024-02-16 18:36:28 +0530

    ML MOdel for college
    ML MOdel by me for college

---
+++ new.py
@@ -0,0 +1,67 @@
+# Import Required Libraries 
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+# Read in white wine data
+white = pd.read_csv("http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv", sep =';')
+
+# Read in red wine data
+red = pd.read_csv("http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv", sep =';')
+
+# First rows of `red`
+red.head()
+
+# Last rows of `white`
+white.tail()
+
+# Take a sample of five rows of `red`
+red.sample(5)
+
+# Describe `white`
+white.describe()
+
+# Double check for null values in `red`
+pd.isnull(red)
+
+# Add `type` column to `red` with price one
+red['type'] = 1
+
+# Add `type` column to `white` with price zero
+white['type'] = 0
+
+# Append `white` to `red`
+wines = red.append(white, ignore_index = True)
+
+# Import `train_test_split` from `sklearn.model_selection`
+from sklearn.model_selection import train_test_split
+X = wines.ix[:, 0:11]
+y = np.ravel(wines.type)
+
+# Splitting the data set for training and validating 
+X_train, X_test, y_train, y_test = train_test_split(
+		X, y, test_size = 0.34, random_state = 45)
+
+
+# Create Histogram
+fig, ax = plt.subplots(1, 2)
+
+ax[0].hist(red.alcohol, 10, facecolor ='red',
+			alpha = 0.5, label ="Red wine")
+
+ax[1].hist(white.alcohol, 10, facecolor ='white',
+		ec ="black", lw = 0.5, alpha = 0.5,
+		label ="White wine")
+
+fig.subplots_adjust(left = 0, right = 1, bottom = 0, 
+			top = 0.5, hspace = 0.05, wspace = 1)
+
+ax[0].set_ylim([0, 1000])
+ax[0].set_xlabel("Alcohol in % Vol")
+ax[0].set_ylabel("Frequency")
+ax[1].set_ylim([0, 1000])
+ax[1].set_xlabel("Alcohol in % Vol")
+ax[1].set_ylabel("Frequency")
+
+fig.suptitle("Distribution of Alcohol in % Vol")
+plt.show()
